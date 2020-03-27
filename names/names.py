8c2984f502a8d590b1b97d4b0d838f4e2aff5712
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2020-03-27 11:17:01 -0600

    attempted a stretch in names

--- names/names.py
+++ names/names.py
@@ -21,7 +21,7 @@ duplicates = []  # Return the list of duplicates in this data structure
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 # Original solution has runtime of O(n^2)
-# My solution has a runtime of O(n)
+# My solution has a runtime of O(n + 1) or O(n)
 for name in names_1:
     cache.set(name, name)
 
@@ -32,9 +32,30 @@ for name in names_2:
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
-print(cache.storage.length)
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# start_time = time.time()
+
+# stretch_dups = []
+
+# f = open('names_1.txt', 'r')
+# names_1 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# f = open('names_2.txt', 'r')
+# names_2 = f.read().split("\n")  # List containing 10000 names
+# f.close()
+
+# for name in names_2:
+#     if names_1.count(name) > 0:
+#         stretch_dups.append(name)
+
+# end_time = time.time()
+
+# print("Attempt using list.count")
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"runtime: {end_time - start_time} seconds")
